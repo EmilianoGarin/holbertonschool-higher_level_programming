ddd2c2d08a03098793fa3affaaa1e16065684cb2
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-06 15:24:12 -0700

    fix task 7 and 8

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,11 +2,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    if len(list_a) < len(list_b):
-        for i in range(len(list_b) - len(list_a)):
+    if len(list_a) < 2:
+        for i in range(len(list_a), 2):
             list_a += [0]
-    else:
-        for i in range(len(list_a) - len(list_b)):
+    if len(list_b) < 2:
+        for i in range(len(list_b), 2):
             list_b += [0]
     ret = tuple(sum(x) for x in zip(list_a, list_b))
     return (ret)

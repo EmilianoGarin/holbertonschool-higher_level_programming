5f598cbf13e43065230a0abf9725c8ac433ee497
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-06 14:12:48 -0700

    :d

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in enumerate(matrix):
         for x in enumerate(i[1]):
             if x[0] == len(i[1]) - 1:
-                print("{}".format(x[1]), end="")
+                print("{:d}".format(x[1]), end="")
             else:
-                print("{} ".format(x[1]), end="")
+                print("{:d} ".format(x[1]), end="")
         print()

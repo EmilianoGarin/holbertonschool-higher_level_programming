260195698024cd4996924248d0d37f8932e35b7d
EmilianoGarin <emiliano.garin333@gmail.com>
2023-06-07 11:28:28 -0700

    fix 1 and 2

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        ret = my_list.copy()
-        for x, i in enumerate(my_list):
-            if search == i:
-                ret[x] = replace
+    ret = my_list.copy()
+    for x, i in enumerate(my_list):
+        if search == i:
+            ret[x] = replace
     return (ret)

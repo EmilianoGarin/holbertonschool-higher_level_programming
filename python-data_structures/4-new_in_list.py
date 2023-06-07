c1293154239660564544499519e0d42b8fa2219d
EmilianoGarin <emiliano.garin333@gmail.com>
2023-06-07 09:28:28 -0700

    fix 3 and 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list
+    if my_list:
         if isinstance(idx, int):
             new_list = my_list.copy()
             if idx < len(my_list) and idx >= 0:

86a9f024ee2900daccd148ca4162067368b299b2
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-08 10:59:11 -0700

    task 6 and 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if not a_dictionary:
+        return
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.addend({key:value})

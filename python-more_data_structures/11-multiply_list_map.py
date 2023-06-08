7eb26ce5f3eff1b28602d756a1b0e000063df82c
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-08 14:51:14 -0700

    fix 11 and 12

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    ret = my_list.copy()
-    ret = list(map(lambda x: x * number, my_list))
-    return (ret)
+    if my_list:
+        return (list(map(lambda x: x * number, my_list)))
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
-        return (None)
+        return (0)
     ret = 0
-    ara_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for old_l, l in zip(' ' + roman_string, roman_string):
-        if l in ara_rom:
-            if old_l in ara_rom and ara_rom[l] > ara_rom[old_l]:
-                ret += -ara_rom[old_l] * 2
-            ret += ara_rom[l]
+        if l in rom_int:
+            if old_l in rom_int and rom_int[l] > rom_int[old_l]:
+                ret += -rom_int[old_l] * 2
+            ret += rom_int[l]
+        else:
+            return (0)
     return (ret)

8e96672a41e697349cfe060451b2f91b78c4d8b1
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-23 16:07:54 -0700

    task 10 and 11

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""task 10"""
+
+
+class Student():
+    """Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+
+        ret = {}
+        for i in attrs:
+            if hasattr(self, i):
+                ret[i] = getattr(self, i)
+        return ret

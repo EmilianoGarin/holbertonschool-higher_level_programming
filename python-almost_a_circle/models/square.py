d0ae05132e7b029f8bdf012536f8c67d5ef6761a
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-06-29 15:25:33 -0700

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,3 +21,27 @@ class Square(Rectangle):
     def size(self, value):
         Rectangle.width.fset(self, value)
         Rectangle.height.fset(self, value)
+
+    def update(self, *args, **kwargs):
+        if len(args) == 0:
+            for i, v in kwargs.items():
+                if i == "id":
+                    if type(v) is not None:
+                        self.id = v
+                elif i == "size":
+                    self.size = v
+                elif i == "x":
+                    Rectangle.x.fset(self, v)
+                elif i == "y":
+                    Rectangle.y.fset(self, v)
+        else:
+            for i, v in enumerate(args):
+                if i == 0:
+                    if type(v) is not None:
+                        self.id = v
+                elif i == 1:
+                    self.size = v
+                elif i == 2:
+                    Rectangle.x.fset(self, v)
+                elif i == 3:
+                    Rectangle.y.fset(self, v)

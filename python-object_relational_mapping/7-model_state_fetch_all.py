fcb4a9322f5435c15f2138aa0bb65d29c3823d71
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-07-29 15:13:12 -0700

    task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""task 7:
+lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State)
+    result = query.all()
+
+for row in result:
+    print(f"{row.id}: {row.name}")

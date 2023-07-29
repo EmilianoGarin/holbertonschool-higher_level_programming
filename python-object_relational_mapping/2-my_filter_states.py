e2a540f9e9d496072ae7846e8c16bbf1fae50782
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-07-29 14:28:15 -0700

    task 5

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,11 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    ck = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
-    if not all(c in ck for c in sys.argv[4]):
-        exit()
     txt = "SELECT * FROM states \
-           WHERE name LIKE '%{0}%' \
+           WHERE name LIKE BINARY '%{0}%' \
            ORDER BY id".format(sys.argv[4])
     cur.execute(txt)
     rows = cur.fetchall()
